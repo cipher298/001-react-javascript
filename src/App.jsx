d535905a4727e2cbd7df9982ee25d566c30620b7
Cipher <Cipher>
2024-09-04 21:07:01 +0700

    step 14

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,54 @@
 import Header from './components/layout/header';
 import { Outlet } from 'react-router-dom';
 import axios from './util/axios.customize';
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
+import { AuthContext } from './components/context/auth.context';
+import { Spin } from 'antd';
 
 function App() {
+  const { setAuth, appLoading, setAppLoading } = useContext(AuthContext);
+
   useEffect(() => {
-    const fetchHelloWorld = async () => {
-      const res = await axios.get(`/v1/api`);
-      console.log('>>> check res: ', res);
+    const fetchAccount = async () => {
+      setAppLoading(true);
+
+      const res = await axios.get(`/v1/api/account`);
+      if (res) {
+        setAuth({
+          isAuthenticated: true,
+          user: {
+            name: res.name,
+            email: res.email,
+          },
+        });
+      }
+
+      setAppLoading(false);
     };
 
-    fetchHelloWorld();
+    fetchAccount();
   }, []);
 
   return (
     <div>
-      <Header />
-      <Outlet />
+      {appLoading === true ? (
+        <div
+          style={{
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            color: 'darkred',
+          }}
+        >
+          <Spin />
+        </div>
+      ) : (
+        <>
+          <Header />
+          <Outlet />
+        </>
+      )}
     </div>
   );
 }
